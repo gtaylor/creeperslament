1b8f1e7e9bf683421bb7504a5a6965a778768a07
Greg Taylor <gtaylor@duointeractive.com>
2011-05-23 01:48:39 -0400

    A bunch of additions, tweaks, and improvements.

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -5,10 +5,25 @@ urlpatterns = patterns('',
     url(r'^$',
         TemplateView.as_view(template_name="pages/index.html"),
         name='home'),
+    # Sent this out in a mailing list mail, so already accumulating
+    # backwards-compatibiltiy cruft!
+    # TODO: Remove this after most players have signed up.
     url(r'^get_playing/',
-        TemplateView.as_view(template_name="pages/get_playing.html"),
+        TemplateView.as_view(template_name="pages/registration.html"),
         name='pages-get_playing'),
+    url(r'^registration/',
+        TemplateView.as_view(template_name="pages/registration.html"),
+        name='pages-registration'),
     url(r'^payment_received/',
         TemplateView.as_view(template_name="pages/payment_received.html"),
         name='pages-payment_received'),
+    url(r'^getting_started/',
+        TemplateView.as_view(template_name="pages/getting_started.html"),
+        name='pages-getting_started'),
+    url(r'^useful_resources/',
+        TemplateView.as_view(template_name="pages/useful_resources.html"),
+        name='pages-useful_resources'),
+    url(r'^support/',
+        TemplateView.as_view(template_name="pages/support.html"),
+        name='pages-support')
 )
